78783aedb9b30a541eafd7841f51f2fd243d01ce
Sabbey <sabbey@pop-os.localdomain>
2021-04-12 18:08:52 +0530

    n-group-sabbeysm

--- app/src/main/java/com/android/internshipprogram/MainActivity.java
+++ app/src/main/java/com/android/internshipprogram/MainActivity.java
@@ -1,14 +1,93 @@
 package com.android.internshipprogram;
 
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText dobInput;
+    private Calendar myCalendar;
+    private Spinner gInput;
+    private Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dobInput = findViewById(R.id.dobInput);
+        myCalendar = Calendar.getInstance();
+        gInput = findViewById(R.id.gInput);
+        submitBtn = findViewById(R.id.submit);
+
+        dobConfig();
+        gSpinnerConfig();
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Submit clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    private void gSpinnerConfig()
+    {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_selection_array,R.layout.spinner_hint);
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
+
+        gInput.setAdapter(adapter);
+    }
+
+    private void dobConfig()
+    {
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        dobInput.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(MainActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "dd MMM, yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dobInput.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
